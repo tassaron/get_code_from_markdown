188e0cfc1b14115ca725f35f539a424439aed72a
Brianna Rainey <brianna@tassaron.com>
2026-02-12 20:54:02 -0500

    test custom run method

--- tests/test_run_code_from_markdown.py
+++ tests/test_run_code_from_markdown.py
@@ -8,3 +8,14 @@ def test_runmethod_subprocess_readme(readme):
 
 def test_runmethod_default_readme(readme):
     run_code_from_markdown_blocks(readme)
+
+
+def test_runmethod_custom_readme(readme):
+    output = None
+
+    def custom_run_method(block):
+        nonlocal output
+        output = block.split("\n", 1)[0]
+
+    run_code_from_markdown_blocks(readme, method=custom_run_method)
+    assert output == "from get_code_from_markdown import run_code_from_markdown_blocks"
